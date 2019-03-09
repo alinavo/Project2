90a257e52c071e98baa134bbf0c7358fffe597b6
mikek4 <michaelkatz@Michaels-MacBook-Pro.local>
2019-03-09 13:46:47 -0500

    HTML Routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,12 @@ var db = require("../models");
 var path = require("path");
 
 module.exports = function(app) {
-  // Load index page
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
   // app.get("/", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/main.html"));
+  //   res.sendFile(path.join(__dirname, "../public/html/main.html"));
   // });
 
   app.get("/marketplace", function(req, res) {
