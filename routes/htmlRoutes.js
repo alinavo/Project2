35083677c64850b24a5e968542837df0e95881b5
mikek4 <mike.katz@outlook.com>
2019-03-12 18:43:08 -0400

    HTML Routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,10 @@
 var db = require("../models");
+<<<<<<< Updated upstream
 var path = require("path");
 
+=======
+ var path = require("path");
+>>>>>>> Stashed changes
 module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
