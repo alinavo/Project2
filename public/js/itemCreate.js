b9aa8d5f57af38b55516e7897ef088096a961361
Roman Verner <romverj@gmail.com>
2019-03-14 18:27:02 -0400

    Fixing a few bugs (#51)

--- public/js/itemCreate.js
+++ public/js/itemCreate.js
@@ -1,44 +1,30 @@
-$("#submitBtn").on("click", function (event) {
-    event.preventDefault();
-    console.log("hello part 1!");
+$(document).ready(function(){
+    $("#submitBtn").on("click", function (event) {
+        event.preventDefault();
+        console.log("hello part 1!");
 
-    var itemName = $("#name");
-    var itemLocation = $("#location");
-    var itemCondition = $("select#condition option:checked");
-    var itemDescription = $("#description");
+        var itemName = $("#name");
+        var itemLocation = $("#location");
+        var itemCondition = $("select#condition option:checked");
+        var itemDescription = $("#description");
 
-    var userID = localStorage.getItem("id");
+        var userID = localStorage.getItem("id");
 
-        $("#submitBtn").click(function () {
-            console.log(itemName);
+        var newItem = {
+            post_title: itemName.val().trim(),
+            location: itemLocation.val().trim(),
+            item_condition: itemCondition.val().trim(),
+            post_body: itemDescription.val().trim(),
+            UserId: userID.val().trim(),
+            active_listing: true
+        };
+
+        console.log(newItem);
+
+        $.post("/api/list", newItem, function (data) {
+            console.log(data);
+            window.location.href = "/marketplace";
         });
 
-        $(function () {
-            $(window).scroll(function () {
-                var winTop = $(window).scrollTop();
-                if (winTop >= 30) {
-                    $("body").addClass("sticky-shrinknav-wrapper");
-                } else {
-                    $("body").removeClass("sticky-shrinknav-wrapper");
-                }
-            });
-        });
-
-    var newItem = {
-        post_title: itemName.val().trim(),
-        location: itemLocation.val().trim(),
-        item_condition: itemCondition.val().trim(),
-        post_body: itemDescription.val().trim(),
-        UserId: userID.val().trim(),
-        active_listing: true
-    };
-
-    console.log(newItem);
-
-
-    $.post("/api/list", newItem, function (data) {
-        console.log(data);
-        window.location.href = "/marketplace";
     });
-
 });
